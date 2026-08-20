31cc40d4f6c3e3198e71245a0b7ca97f9718930e
DAGGNO01 <ailearnt001@gmail.com>
2026-08-21 07:19:26 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ import java.util.TimeZone
 import java.util.zip.CRC32
 import kotlin.text.RegexOption.IGNORE_CASE
 
-val applicationId = "org.autojs.autojs6"
+val applicationId = "com.f001.oh"
 val sign = Sign("$rootDir/sign.properties")
 val versions = Versions("$rootDir/version.properties")
 
